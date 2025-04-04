ac6dec4f1c6426101dcb3cf62e5abfb5be73e983
Futureppo <luominzhi2005@qq.com>
2025-04-04 14:10:54 +0800

    每一张图片一条消息，并在合并转发内防止刷屏

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from astrbot.api import logger
 from astrbot.api.message_components import *
 from astrbot.api.event import filter, AstrMessageEvent
 import os
+import re
+import aiohttp
+import json
 
-
-@register("astrbot_plugin_custom_menu", "Futureppo", "自定义图片菜单", "1.0.0")
+@register("astrbot_plugin_custom_menu", "Futureppo", "自定义图片菜单。更新前记得备份你的图片！！！", "1.0.0")
 class custommenu(Star):
     def __init__(self, context: Context):
         super().__init__(context)
@@ -40,17 +42,30 @@ class custommenu(Star):
             logger.warning(f"menu文件夹中没有找到任何图片: {menu_dir}")
             return
 
-        images = []
-        for path in image_paths:
-            if os.path.exists(path):
-                logger.debug(f"成功加载图片: {path}")
-                images.append(Image.fromFileSystem(path))
-            else:
+        nodes_list = []
+        for idx, path in enumerate(image_paths):
+            if not os.path.exists(path):
                 logger.info(f"图片不存在: {path}")
+                continue
 
-        node = Node(
-            name="菜单",
-            content=images
-        )
+            # 创建伪造的用户信息
+            # 咕咕咕   qq_number = f"10000{idx}"  # 伪造的QQ号
+            nickname = f"菜单"  # 伪造的昵称
 
-        yield event.chain_result([node])
+            # 加载图片
+            image = Image.fromFileSystem(path)
+            logger.debug(f"成功加载图片: {path}")
+
+            # 创建节点
+            node = Node(
+                # uin=int(qq_number),
+                name=nickname,
+                content=[image]
+            )
+            nodes_list.append(node)
+        # 发送
+        if nodes_list:
+            nodes = Nodes(nodes=nodes_list)
+            yield event.chain_result([nodes])
+        else:
+            yield event.plain_result("发送失败，请检查menu文件夹中的图片。")
